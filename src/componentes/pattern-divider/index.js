86fff1df9f20baeed25ce679ab397dfa2e9e9c54
Allesson Figueiredo <allfigueiredo@outlook.com>
2022-08-31 00:40:57 -0300

    Teste do pattern-divider

--- src/componentes/pattern-divider/index.js
+++ src/componentes/pattern-divider/index.js
@@ -2,7 +2,8 @@ import './pattern-divider.css'
 
 function PatternDivider () {
     return (
-        <img className='pattern-divider' src='../../../imagens/pattern-divider-mobile.svg' alt='pattern-divider'></img>
+        // <img className='pattern-divider' src='../../../imagens/pattern-divider-mobile.svg' alt='pattern-divider'></img>
+        <div className='pattern-divider'></div>
     )
 }
 
